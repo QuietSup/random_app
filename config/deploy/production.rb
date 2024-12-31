64158dcbf6ff8d31271cc99347cd3a3179991272
QuietSup <feeling_of_infinity@outlook.com>
2024-12-31 17:02:25 +0200

    fix: remove db role from capistrano

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -20,7 +20,7 @@
 # role :app, %w{deploy@example.com}, my_property: :my_value
 # role :web, %w{user1@primary.com user2@additional.com}, other_property: :other_value
 # role :db,  %w{deploy@example.com}
-role :db,  %w[random-app-db.c9y0ooyy4wdu.eu-north-1.rds.amazonaws.com]
+# role :db,  %w[random-app-db.c9y0ooyy4wdu.eu-north-1.rds.amazonaws.com]
 
 
 # Configuration
